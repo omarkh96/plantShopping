10a4bbd09170aa4bf88a1d6b5980aed41fa93a5b
omarkh96 <omarkhweis9641@gmail.com>
2026-02-19 16:52:26 +0100

    Create cartslice.jsx

---
+++ cartslice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++; // Increment quantity if item already exists
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 }); // Add new item with quantity 1
+      }
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const item = state.items.find(item => item.name === name);
+      if (item) {
+        item.quantity = quantity; // Update the quantity of the item
+      }
+    
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
